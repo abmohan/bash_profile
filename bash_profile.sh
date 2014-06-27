85da2cda5444ef3b44bf49820f62d6c01acf59e3
Ashwin Balamohan <abmohan@gmail.com>
2014-06-26 21:54:36 -0400

    initial commit

---
+++ bash_profile.sh
@@ -0,0 +1,11 @@
+export PS1="\[\033[36m\]\u\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias ls='ls -GFh'
+
+# Tell grep to highlight matches
+export GREP_OPTIONS='--color=auto'
+
+export PATH=/usr/local/bin:$PATH
+ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
